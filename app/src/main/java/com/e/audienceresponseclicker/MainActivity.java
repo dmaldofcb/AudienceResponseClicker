3134d95845089ca46724ba0149cfc60857f928b3
dmaldofcb <damald1@umbc.edu>
2019-04-15 15:14:45 -0400

    Added the register UI

--- app/src/main/java/com/e/audienceresponseclicker/MainActivity.java
+++ app/src/main/java/com/e/audienceresponseclicker/MainActivity.java
@@ -1,13 +1,47 @@
 package com.e.audienceresponseclicker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "BUGS";
+    private EditText username;
+    private EditText password;
+    private Button login;
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
+        login = (Button) findViewById(R.id.login);
+        register = (Button) findViewById(R.id.register);
+
+        login.setOnClickListener(this);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login:
+                Log.d(TAG, "Pressed Login Button");
+                break;
+
+            case R.id.register:
+                Intent createAccount = new Intent(MainActivity.this, Register.class);
+                startActivity(createAccount);
+                break;
+        }
     }
 }
